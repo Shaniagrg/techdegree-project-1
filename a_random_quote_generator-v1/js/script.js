c76d05aa68eea6ff0b8efd6fc3e56b50fb85d461
Shaniagrg <sglearner2022@gmail.com>
2026-09-08 04:12:57 -0700

    Created an Array to Store Quotes

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -9,8 +9,47 @@ project 1 - A Random Quote Generator
 
 /*** 
  * `quotes` array 
+ * It consists of property quote, source, year, citation, tags
 ***/
 
+const quotes = [
+  {
+    quote: "The future depends on what you do today",
+    source: "Mahatma Gandhi",
+    tags: ["History","Inspiration"]
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    source: "Steve Jobs",
+    year: 2005
+  },
+  {
+    quote: "It always seems impossible until it's done",
+    source: "Nelson Mandela",
+    citation: "https://www.nelsonmandela.org/",
+    citationName: "Long Walk to Freedom",
+    year: 2001
+  },
+  {
+    quote: "The only limit to our realization of tomorrow is our doubts of today.",
+    source: "Franklin D. Roosevelt"
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    source: "Theodore Roosevelt"
+  },
+  {
+    quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    source: "Winston Churchill",
+    citation: "https://www.winstonchurchill.org/",
+    citationName: "Speech to the House of Commons",
+    year: 1949
+  },
+  {
+    quote: "The journey of a thousand miles begins with one step.",
+    source: "Lao Tzu"
+  }
+]
 
 
 /***
